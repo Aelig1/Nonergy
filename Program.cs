9e763a9cdcd9ccef8b8cb279412f27b8153ac597
Aapo Hakamaa <aapo.hakamaa@student.tut.fi>
2018-03-15 10:45:02 +0200

    Initialize C# project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nonergy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
